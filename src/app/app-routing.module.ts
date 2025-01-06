741dd2e9d7aada16b0f59b48b74a58b84185d9b0
KomaromiJano <komaromijano@gmail.com>
2025-01-06 20:34:29 +0100

    Begin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { RentComponent } from './rent/rent.component';
+import { OrderComponent } from './order/order.component';
+import { ClubCardComponent } from './club-card/club-card.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home", component: HomeComponent
+  },
+  {
+    path: "contact", component: ContactComponent
+  },
+  {
+    path: "rent", component: RentComponent
+  },
+  {
+    path: "order", component: OrderComponent
+  },
+  {
+    path: "clubcart", component: ClubCardComponent 
+  },
+  {
+    path: "login", component: LoginComponent 
+  },
+  {
+    path: "register", component: RegisterComponent, 
+  },  
+  {
+    path: "", redirectTo: "home", pathMatch: "full"
+  },
+  {
+    path: "**", component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactComponent } from './contact/contact.component';
+import { ClubCardComponent } from './club-card/club-card.component';
+import { RentComponent } from './rent/rent.component';
+import { OrderComponent } from './order/order.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContactComponent,
+    ClubCardComponent,
+    RentComponent,
+    OrderComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
