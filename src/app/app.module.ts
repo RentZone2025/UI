f91672c197e10669f3cda72fa93958bbbd889782
BencsikBoglarBianka <bencsikboglarbianka@ktch.hu>
2025-03-20 11:13:59 +0100

    LittleDet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-
-
-import { provideHttpClient } from '@angular/common/http';
-
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
